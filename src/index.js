36f4354592f9c984fb9c5c2c33995a18b5b89a85
Simon Oro <simonoro1@outlook.com>
2022-05-21 15:01:59 -0300

    port

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new socketio.Server(server)
 
-const port = 3000
+const port = process.env.PORT || 3000 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
